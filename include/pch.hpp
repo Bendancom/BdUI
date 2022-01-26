b5f20f01f27ecaa557bec6ea3e9b7ae241c885d1
Bendancom <1317858038@qq.com>
2022-01-27 01:13:51 +0800

    Big Change in the Event and Attribute,and we new the EventList class

--- include/pch.hpp
+++ include/pch.hpp
@@ -76,12 +76,19 @@ class Event<Return(Param...)> : public std::vector<Delegate<Return(Param...)>>{
 public:
     using std::vector<Delegate<Return(Param...)>>::vector;
     using std::vector<Delegate<Return(Param...)>>::operator=;
-    std::map<std::type_index,Return> operator()(Param... args){
+    void SetCheckFunc(bool (*checkptr)(Param...)) { check = checkptr; };
+    void SetReturnCallBackFunc(void (*returncallbackptr)(const std::map<std::type_index,Return>&)) { returncallback = returncallbackptr; }
+    void operator()(Param... args){
+        if (check != nullptr){
+            if (!check(args...)) return;
+        }
+        else if (!Check(args...)) return;
         std::map<std::type_index,Return> temp;
         for (auto iter = this->cbegin(); iter != this->cend(); iter++){
             temp.insert(std::pair<std::type_index,Return>((*iter).target_type(),(*iter)(args...)));
         }
-        return temp;
+        if (returncallback != nullptr) returncallback(temp);
+        else ReturnCallBack(temp);
     }
     Event<Return(Param...)> &operator+=(const Delegate<Return(Param...)> &d){
         if (this->size() == 0 || (*(std::find(this->begin(),this->end(),d))) != d) this->push_back(d);
@@ -93,13 +100,23 @@ public:
         if ((*iter) == d) this->erase(iter);
         return *this;
     }
+private:
+    virtual bool Check(Param...) { return true; }
+    virtual void ReturnCallBack(const std::map<std::type_index,Return>&) { return; }
+    bool (*check)(Param...) = nullptr;
+    void (*returncallback)(const std::map<std::type_index,Return>&) = nullptr;
 };
 template<typename... Param>
 class Event<void(Param...)> : public std::vector<Delegate<void(Param...)>>{
     public:
     using std::vector<Delegate<void(Param...)>>::vector;
     using std::vector<Delegate<void(Param...)>>::operator=;
+    void SetCheckFunc(bool (*checkptr)(Param...)) { check = checkptr; };
     void operator()(const Param &... args){
+        if (check != nullptr){
+            if (!check(args...)) return;
+        }
+        else if (!Check(args...)) return;
         for (auto iter = this->begin();iter != this->end();iter++) (*iter)(args...);
     }
     Event<void(Param...)> &operator+=(const Delegate<void(Param...)> &d){
@@ -112,27 +129,65 @@ class Event<void(Param...)> : public std::vector<Delegate<void(Param...)>>{
         if ((*iter) == d) this->erase(iter);
         return *this;
     }
+private:
+    virtual bool Check(Param...) { return true; }
+    bool (*check)(Param...) = nullptr;
+};
+
+template<typename Return,typename... Param> class EventList;
+template<typename Return,typename... Param>
+class EventList<Return(Param...)> : public std::vector<Event<Return(Param...)>>{
+public:
+    using std::vector<Event<Return(Param...)>>::vector;
+    using std::vector<Event<Return(Param...)>>::operator=;
+    void operator()(Param... args){
+        for (auto iter = this->begin();iter != this->end();iter++) (*iter)(args...);
+    }
+    EventList<Return(Param...)> &operator+=(const Event<Return(Param...)> &e){
+        if (this->size() == 0 || (*(std::find(this->begin(),this->end(),e))) != e) this->push_back(e);
+        return *this;
+    }
+    EventList<Return(Param...)> &operator-=(const Event<Return(Param...)> &e){
+        if(this->size() == 0) return *this;
+        auto iter = std::find(this->begin(),this->end(),e);
+        if ((*iter) == e) this->erase(iter);
+        return *this;
+    }
 };
 
 template<typename T>
-class Attribute : public std::atomic<T>{
+class Attribute : private std::atomic<T>{
 public:
-    Event<void(T)> Changed;
-    Attribute(T value) {this->store(value);}
-    Attribute(const Attribute<T> &a) : Changed(a.Changed) {this->store(a.load());}
-    operator T() { return this->load(); }
+    EventList<void(T)> Eventlist;
+    Attribute(T value,T (*get)(T) = nullptr,bool (*set)(T&) = nullptr) : 
+        getptr(get),setptr(set) {this->store(value);}
+    Attribute(const Attribute<T> &a,T (*get)(T) = nullptr,bool (*set)(T&) = nullptr) : 
+        Eventlist(a.Eventlist),getptr(get),setptr(set) {this->store(a.load());}
+    operator T() { 
+        if (getptr != nullptr) return getptr(this->load());
+        else return get(this->load());
+    }
     const T operator->() { return this->load(); }
     Attribute<T> &operator=(T value){
+        if (setptr != nullptr) { if (!setptr(value)) return *this; }
+        else { if (!set(value)) return *this; }
         this->store(value);
-        Changed(value);
+        Eventlist(value);
         return *this;
     }
     Attribute<T> &operator=(const Attribute<T> &a){
         T value = a.load();
+        if (setptr != nullptr) { if (!setptr(value)) return *this; }
+        else { if (!set(value)) return *this; }
         this->store(value);
-        Changed(value);
+        Eventlist(value);
         return *this;
     }
+private:
+    virtual T get(T t) { return t; };
+    virtual bool set(T&) { return true; };
+    T (*getptr)(T);
+    bool (*setptr)(T&);
 };
 
 struct Point{
