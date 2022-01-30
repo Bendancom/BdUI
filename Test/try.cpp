e761fd66b4b8af1a97101796d2e72a8610b8bc74
yueyinqiu <lujiaqi2003@126.com>
2022-01-30 14:05:55 +0800

    and better

--- Test/try.cpp
+++ Test/try.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 using namespace BdUI;
 
-int main(){
-    Window* window = new Window("HHHaaaaaaa",{100,100},{500,800});
+int main()
+{
+    Window *window = new Window("HHHaaaaaaa", {100, 100}, {500, 800});
     std::cin.get();
 }
